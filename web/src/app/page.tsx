fdc7f008ed8534fb982875cf13e9585e522dbb3e
Marcelo <marcelo.arraes@gogood.com.br>
2023-06-16 11:55:44 +0930

    first commit

---
+++ web/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  const something = "some";
+  const elseing = "elsing";
+  return (
+    <h1 className="h-screen bg-zinc-950 text-zinc-50">
+      Hello World {something}
+      {elseing}
+    </h1>
+  );
+}
