569b878a6451e38499800dbb278502de55974a03
Marcelo <marcelo.arraes@gogood.com.br>
2023-06-19 19:50:35 +0930

    feat: added images, background and packages to mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,77 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import {
+  ImageBackground,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+// import Synth1 from "./src/assets/synth1.jpeg";
+import Synth2 from "./src/assets/synth2.jpeg";
+// import Synth3 from "./src/assets/synth3.jpeg";
+// import Synth4 from "./src/assets/synth4.jpeg";
+// import Synth5 from "./src/assets/synth5.jpeg";
+import Stripes from "./src/assets/stripe.svg";
+import { styled } from "nativewind";
+import BgSun from "./src/assets/bg-sun.png";
+
+const StyledStripes = styled(Stripes);
 
 export default function App() {
+  const [hasLoaded, err] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+  if (!hasLoaded) {
+    return null;
+  }
+  if (err != null) {
+    console.log("error123", err);
+  }
   return (
-    <View className="flex-1 items-center justify-center bg-zinc-800">
-      <Text className="text-5xl font-bold text-zinc-200">Hello World</Text>
-      <StatusBar style="light" />
-    </View>
+    <ImageBackground
+      source={Synth2}
+      className="relative flex-1 items-center bg-zinc-800 py-10"
+      imageStyle={{ position: "absolute", opacity: 0.7 }}
+    >
+      <StyledStripes
+        className="absolute right-48 top-96 rotate-90"
+        height={"5%"}
+      />
+      <View className="flex-1 items-center justify-center gap-6">
+        <Image
+          source={BgSun}
+          alt="Sol Invictus"
+          style={{ width: 100, height: 100, position: "absolute", top: "20%" }}
+        />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-zinc-50">
+            Your time Capsule
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-zinc-100">
+            Save the best moments of your life.
+          </Text>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="justify-center rounded-full bg-green-500 px-5 py-3"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            SAVE MEMORY
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Made by Marcelo, with love.
+      </Text>
+      <StatusBar style="light" translucent />
+    </ImageBackground>
   );
 }
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,14 +1,59 @@
+import Image from "next/image";
+import { User } from "lucide-react";
+import sunSynthware from "../assets/clipart2580163.png";
+
 export default function Home() {
   return (
-    <main className="grid min-h-screen grid-cols-2">
+    <main className="grid min-h-screen grid-cols-2 bg-[url(../assets/bg-starts.svg)]">
       {/* Left */}
-      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-starts.svg)] px-28 py-16">
-        <div className="absulute right-0 top-1/2 h-[288px] w-[526px] translate-x-1/2 translate-y-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 px-28 py-16">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-80 blur-full" />
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+        {/* Sign In */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-500"
+        >
+          <div className="h-18 w-18 flex items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+          <p className=" max-w-[140px] text-sm leading-snug">
+            <span className="underline">Create your account</span> and save your
+            memories!
+          </p>
+        </a>
+        {/* Hero */}
+        <div className="space-y-5 ">
+          <Image src={sunSynthware} alt="image" width={100} height={100} />
+          <div className="max-w-[420px]">
+            <h1 className="mt-5 text-5xl font-bold leading-tight text-gray-50">
+              Your Time Capsule
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Track your best moments and share them!
+            </p>
+          </div>
+          <a
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm font-bold uppercase leading-none text-black hover:bg-green-600"
+            href="http://"
+          >
+            RECORD MEMORY
+          </a>
+        </div>
+        {/* foot */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          <a
+            target="_blank"
+            rel="nonreferer"
+            className="underline hover:text-gray-100"
+            href="https://marcelosportfolio.vercel.app/"
+          >
+            Marcelo&apos;s project{" "}
+          </a>
+        </div>
       </div>
-
       {/* Right */}
-      <div className="flex flex-col items-center justify-center p-16">
+      <div className="flex flex-col items-center justify-center  p-16">
         <p className="w-[360px] text-center leading-relaxed">
           You didn&apos;t register any memory. {" \n"}
           <a href="" className="underline hover:text-gray-500">
